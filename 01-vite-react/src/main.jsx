12d455c2f84021c775e4ccd7682d76e407259c7f
AnnanyaSwamy <swamyannanya753@gmail.com>
2024-08-23 21:04:52 +0530

    Components

--- 01-vite-react/src/main.jsx
+++ 01-vite-react/src/main.jsx
@@ -1,9 +1,22 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+const customElement=<a href='https://google.com' target='_blank' >Visit</a>
+//OR
+const customElementTwo=React.createElement(
+  'a',
+  {
+    href:'https://google.com',
+    target:'_blank'
+  },
+  'Click me to visit Google'
+)
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  //customElement
+  customElementTwo
 )
